c57535bbd329a967496668a7f24c69cf7f9a661b
mush <btyh17mxy@gmail.com>
2014-04-23 22:09:20 +0800

    f

--- index/urls.py
+++ index/urls.py
@@ -10,6 +10,5 @@ urlpatterns = patterns('index.views',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
